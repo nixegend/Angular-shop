814734d54421e4edb063f85b5ce21524b4371212
Antony <nixegend@gmail.com>
2015-02-21 23:28:17 +0200

    add basket btn

--- js/controllers/HeaderCtrl.js
+++ js/controllers/HeaderCtrl.js
@@ -3,6 +3,10 @@ define(['app', 'directHeader', 'isActiveLink', 'bootstrap', 'RCMservice', 'APIse
 	app.controller('HeaderCtrl', ['$scope', 'RCMservice', 'APIservice',
         function ($scope, RCMservice, APIservice) {
 
+        $scope.langModel = 'en';
+        $scope.navMenu = 'partials/nav-menu.html';
+        $scope.basket = 'svg/basket.svg';
+
         APIservice.getJSONresponse('menu').then(function (response) {
             $scope.menu = RCMservice.reConstructor(response);
         });
